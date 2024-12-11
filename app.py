9479c147219f77c98bfeb7b4a665566b5e1b097c
Mihaly24 <fairuzk20@gmail.com>
2024-12-11 21:32:00 +0700

    Menambahkan nanya umur

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    age = None
     if request.method == 'POST':
         name = request.form['name']
-        return render_template('index.html', name=name)
+        age = request.form['age']
+        return render_template('index.html', name=name, age=age)
     return render_template('index.html')
 
 if __name__ == '__main__':
